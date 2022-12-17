68bd0b1d31f88302b1e7dbe70991babd3abd6cfc
Roman Levin <romanvalevin@gmail.com>
2022-12-17 04:39:55 +0300

    modified tests

--- main.cpp
+++ main.cpp
@@ -23,69 +23,101 @@ int main() {
 
 
     bool printEnabled = 0;
-    int n = 10000;
-    int hu[n];
-    for (int i = 0; i < n; i++){
-        hu[i] = std::rand();
-        //std::cout << hu[i] << " ";
+    bool expMode = 0;
+    bool time_n_mode = 1;
+
+    int n = 100000;
+    int dn = 5000;
+    int n_max = n+dn;
+
+    while(n < n_max) {
+
+        int hu[n];
+        for (int i = 0; i < n; i++) {
+            hu[i] = std::rand();
+            //std::cout << hu[i] << " ";
+        }
+
+        printf("%8d ", n);
+
+        ArraySequence<int> arraySequence1(hu, n);
+        ArraySequence<int> arraySequence2(hu, n);
+        ArraySequence<int> arraySequence3(hu, n);
+
+        begin = std::chrono::steady_clock::now();
+        Sorter<int, DynamicArray<int>::Iterator>::BubbleSort(arraySequence1.begin(), arraySequence1.end(), com);
+        end = std::chrono::steady_clock::now();
+        elapsed_ms = std::chrono::duration_cast<std::chrono::milliseconds>(end - begin);
+        if (time_n_mode)
+            printf("%6d ", elapsed_ms.count());
+        if (expMode)
+            std::cout << "BubbleSort Array: " << elapsed_ms.count() << " ms\n";
+        if (printEnabled)
+            Experiments<typename DynamicArray<int>::Iterator>::print(arraySequence1.begin(), arraySequence1.end());
+
+        begin = std::chrono::steady_clock::now();
+        Sorter<int, DynamicArray<int>::Iterator>::QuickSort(arraySequence2.begin(), arraySequence2.end(), com);
+        end = std::chrono::steady_clock::now();
+        elapsed_ms = std::chrono::duration_cast<std::chrono::milliseconds>(end - begin);
+        if (time_n_mode)
+            printf("%6d ", elapsed_ms.count());
+        if (expMode)
+            std::cout << "QuickSort Array: " << elapsed_ms.count() << " ms\n";
+        if (printEnabled)
+            Experiments<typename DynamicArray<int>::Iterator>::print(arraySequence2.begin(), arraySequence2.end());
+
+        begin = std::chrono::steady_clock::now();
+        Sorter<int, DynamicArray<int>::Iterator>::MergeSort(arraySequence3.begin(), arraySequence3.end(), com);
+        end = std::chrono::steady_clock::now();
+        elapsed_ms = std::chrono::duration_cast<std::chrono::milliseconds>(end - begin);
+        if (time_n_mode)
+            printf("%6d ", elapsed_ms.count());
+        if (expMode)
+            std::cout << "MergeSort Array: " << elapsed_ms.count() << " ms\n";
+        if (printEnabled)
+            Experiments<typename DynamicArray<int>::Iterator>::print(arraySequence3.begin(), arraySequence3.end());
+
+        LinkedListSequence<int> list1(hu, n);
+        LinkedListSequence<int> list2(hu, n);
+        LinkedListSequence<int> list3(hu, n);
+
+
+        begin = std::chrono::steady_clock::now();
+        Sorter<int, LinkedList<int>::Iterator>::BubbleSort(list1.begin(), list1.end(), com);
+        end = std::chrono::steady_clock::now();
+        elapsed_ms = std::chrono::duration_cast<std::chrono::milliseconds>(end - begin);
+        if (time_n_mode)
+            printf("%6d ", elapsed_ms.count());
+        if (expMode)
+            std::cout << "BubbleSort List: " << elapsed_ms.count() << " ms\n";
+        if (printEnabled)
+            Experiments<typename LinkedList<int>::Iterator>::print(list1.begin(), list1.end());
+
+        begin = std::chrono::steady_clock::now();
+        Sorter<int, LinkedList<int>::Iterator>::QuickSort(list2.begin(), list2.end(), com);
+        end = std::chrono::steady_clock::now();
+        elapsed_ms = std::chrono::duration_cast<std::chrono::milliseconds>(end - begin);
+        if (time_n_mode)
+            printf("%6d ", elapsed_ms.count());
+        if (expMode)
+            std::cout << "QuickSort List: " << elapsed_ms.count() << " ms\n";
+        if (printEnabled)
+            Experiments<typename LinkedList<int>::Iterator>::print(list2.begin(), list2.end());
+
+        begin = std::chrono::steady_clock::now();
+        Sorter<int, LinkedList<int>::Iterator>::MergeSort(list3.begin(), list3.end(), com);
+        end = std::chrono::steady_clock::now();
+        elapsed_ms = std::chrono::duration_cast<std::chrono::milliseconds>(end - begin);
+        if (time_n_mode)
+            printf("%6d ", elapsed_ms.count());
+        if (expMode)
+            std::cout << "MergeSort List: " << elapsed_ms.count() << " ms\n";
+        if (printEnabled)
+            Experiments<typename LinkedList<int>::Iterator>::print(list3.begin(), list3.end());
+
+        std::cout << std::endl;
+
+        n += dn;
     }
-
-    ArraySequence<int> arraySequence1(hu, n);
-    ArraySequence<int> arraySequence2(hu, n);
-    ArraySequence<int> arraySequence3(hu, n);
-
-    begin = std::chrono::steady_clock::now();
-    Sorter<int, DynamicArray<int>::Iterator>::BubbleSort(arraySequence1.begin(), arraySequence1.end(), com);
-    end = std::chrono::steady_clock::now();
-    elapsed_ms = std::chrono::duration_cast<std::chrono::milliseconds>(end - begin);
-    std::cout << "BubbleSort Array: " << elapsed_ms.count() << " ms\n";
-    if (printEnabled)
-        Experiments<typename DynamicArray<int>::Iterator>::print(arraySequence1.begin(), arraySequence1.end());
-
-    begin = std::chrono::steady_clock::now();
-    Sorter<int, DynamicArray<int>::Iterator>::QuickSort(arraySequence2.begin(), arraySequence2.end(), com);
-    end = std::chrono::steady_clock::now();
-    elapsed_ms = std::chrono::duration_cast<std::chrono::milliseconds>(end - begin);
-    std::cout << "QuickSort Array: " << elapsed_ms.count() << " ms\n";
-    if (printEnabled)
-        Experiments<typename DynamicArray<int>::Iterator>::print(arraySequence2.begin(), arraySequence2.end());
-
-    begin = std::chrono::steady_clock::now();
-    Sorter<int, DynamicArray<int>::Iterator>::MergeSort(arraySequence3.begin(), arraySequence3.end(), com);
-    end = std::chrono::steady_clock::now();
-    elapsed_ms = std::chrono::duration_cast<std::chrono::milliseconds>(end - begin);
-    std::cout << "MergeSort Array: " << elapsed_ms.count() << " ms\n";
-    if (printEnabled)
-        Experiments<typename DynamicArray<int>::Iterator>::print(arraySequence3.begin(), arraySequence3.end());
-
-    LinkedListSequence<int> list1(hu, n);
-    LinkedListSequence<int> list2(hu, n);
-    LinkedListSequence<int> list3(hu, n);
-
-
-    begin = std::chrono::steady_clock::now();
-    Sorter<int, LinkedList<int>::Iterator>::BubbleSort(list1.begin(), list1.end(), com);
-    end = std::chrono::steady_clock::now();
-    elapsed_ms = std::chrono::duration_cast<std::chrono::milliseconds>(end - begin);
-    std::cout << "BubbleSort List: " << elapsed_ms.count() << " ms\n";
-    if (printEnabled)
-        Experiments<typename LinkedList<int>::Iterator>::print(list1.begin(), list1.end());
-
-    begin = std::chrono::steady_clock::now();
-    Sorter<int, LinkedList<int>::Iterator>::QuickSort(list2.begin(), list2.end(), com);
-    end = std::chrono::steady_clock::now();
-    elapsed_ms = std::chrono::duration_cast<std::chrono::milliseconds>(end - begin);
-    std::cout << "QuickSort List: " << elapsed_ms.count() << " ms\n";
-    if (printEnabled)
-        Experiments<typename LinkedList<int>::Iterator>::print(list2.begin(), list2.end());
-
-    begin = std::chrono::steady_clock::now();
-    Sorter<int, LinkedList<int>::Iterator>::MergeSort(list3.begin(), list3.end(), com);
-    end = std::chrono::steady_clock::now();
-    elapsed_ms = std::chrono::duration_cast<std::chrono::milliseconds>(end - begin);
-    std::cout << "MergeSort List: " << elapsed_ms.count() << " ms\n";
-    if (printEnabled)
-        Experiments<typename LinkedList<int>::Iterator>::print(list3.begin(), list3.end());
-
     return 0;
 }
